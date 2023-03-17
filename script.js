1843795ee438da30542f4dcd31e6c661f3bde16d
Yash Kolambekar <122073042+yashkjsomaiya@users.noreply.github.com>
2023-03-17 21:18:03 +0530

    Version 1
    Uploading the first ever polished version of nazzmusic.me

---
+++ script.js
@@ -0,0 +1,10 @@
+// const openInYoutube = document.getElementById("yt-bt-div");
+
+// var num = 0;
+
+// openInYoutube.addEventListener("click", function(){
+//     num++;
+//     setTimeout(()=>{
+//         alert(`Clicked ${num} times`);
+//     },200);
+// })
