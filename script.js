b57cc4d18f412a232faab45c8163ce8c4de26009
Yash Kolambekar <122073042+yashkjsomaiya@users.noreply.github.com>
2023-03-19 14:38:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -13,4 +13,10 @@ document.body.onload = function(){
     document.getElementById("read-more-div").addEventListener("click", function(){
         window.open("/biography", "_self");
     })
+
+    const yash = document.getElementById("yash");
+    yash.addEventListener("click", function(){
+        yash.style.boxShadow = "0px 5px 10000px 5000px var(--color-6-1)";
+        window.open("https://yash.codes", "_self");
+    })
 }
